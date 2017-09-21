3681679e1717a32435c783d90597c485794721bc
Nick Veys <nick@codelever.com>
2017-09-21 11:20:16 -0500

    Split root heroes component from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
-
 @Component({
-  selector: 'toh-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'toh-app',
+  templateUrl: 'app.component.html'
 })
 export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
-  heroes: Hero[];
-  selectedHero: Hero;
 
-  constructor(private heroService: HeroService) {
-    /* do nothing */
+  constructor() {
+
   }
 
-  ngOnInit(): void {
-    this.getHeroes();
-  }
+  ngOnInit() {
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  private getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 }
---
+++ src/app/heroes.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+@Component({
+  selector: 'toh-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService) {
+    /* do nothing */
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  private getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+}
